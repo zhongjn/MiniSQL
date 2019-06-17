12aee3626281527b9a17395b8106f2882f2f6718
610223674@qq.com <610223674@qq.com>
2019-06-17 20:03:44 +0800

    insert update delete

--- query_parser.h
+++ query_parser.h
@@ -23,7 +23,7 @@ struct SelectSource {
 
 struct InsertStatement : public Statement {
     string into;
-    vector<string> fields;
+    Nullable<vector<string>> fields;
     vector<pair<Type, Value>> values;
 };
 
@@ -38,12 +38,21 @@ struct SelectStatement : public Statement {
     unique_ptr<Expression> where;
 };
 
+struct UpdateField {
+	string item;
+	unique_ptr<Expression> expr;
+};
+
 struct UpdateStatement : public Statement {
-    // TODO
+	string relation;
+	vector<UpdateField> set;
+	unique_ptr<Expression> where;
 };
 
 struct DeleteStatement : public Statement {
-    // TODO
+	string relation;
+	unique_ptr<Expression> where;
+
 };
 
 class QueryParser {
@@ -89,11 +98,32 @@ private:
         return expression(expr, a, b);
     }
 
+	bool insert_source(string& source);
+	bool insert_field(string& field);
+	bool insert_value(pair<Type, Value>& value);
+	bool insert_list(vector<string>& fields);
+	bool insert_list(vector<pair<Type, Value>>& values);
+	bool insert_stmt_inner(unique_ptr<InsertStatement>& stmt);
+	bool insert_stmt(unique_ptr<Statement>& stmt);
+
     bool select_field(SelectField& f);
     bool select_list(vector<SelectField>& fields);
     bool select_source(SelectSource& source);
     bool select_stmt_inner(unique_ptr<SelectStatement>& stmt);
     bool select_stmt(unique_ptr<Statement>& stmt);
+
+	bool update_source(string& source);
+	bool update_field(UpdateField& f);
+	bool update_list(vector<UpdateField> & fields);
+	bool update_stmt_inner(unique_ptr<UpdateStatement> & stmt);
+	bool update_stmt(unique_ptr<Statement>& stmt);
+
+	bool delete_source(string& source);
+	bool delete_stmt_inner(unique_ptr<DeleteStatement>& stmt);
+	bool delete_stmt(unique_ptr<Statement>& stmt);
+
+	bool table_stmt(unique_ptr<Statement>& stmt);
+	bool index_stmt(unique_ptr<Statement>& stmt);
 
     bool stmt(unique_ptr<Statement>& s);
 
